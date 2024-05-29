7da37efeb5e422a6b415dc3ccf17436f2eb38f7f
Jorge Miranda de la Quintana <70879770+El-Mito-de-Giralda@users.noreply.github.com>
2024-05-29 10:56:05 +0200

    Delete Ejercicios Html/src/git3.js

--- Ejercicios Html/src/git3.js
+++
@@ -1,7 +0,0 @@
-function addelement() {
-let Myimg= document.createElement("img");
-Myimg.src= "C:\Users\miran\OneDrive\Escritorio\GG\Sass\src\images\logo.jpg";
-document.body.appendChild(img);
-const src =document.querySelector("#imgPortada");
-document.body.insertBefore(img, src);}
-
